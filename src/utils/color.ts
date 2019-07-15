1c2363dc320412a53fd60c7cf7cfe409b51cfdb1
Titof <lanyanfockchristophe@gmail.com>
2019-07-15 09:50:49 +0200

    Update color.ts

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,14 +1,7 @@
+import { MD5 } from "./MD5";
+
 var stringToColour = function(str: string | String) {
-    var hash = 0;
-    for (var i = 0; i < str.length; i++) {
-      hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    var colour = '#';
-    for (var i = 0; i < 3; i++) {
-      var value = (hash >> (i * 8)) & 0xFF;
-      colour += ('00' + value.toString(16)).substr(-2);
-    }
-    return colour;
+    return '#' + MD5(str).slice(0, 6);
 }
 
 export { stringToColour }
